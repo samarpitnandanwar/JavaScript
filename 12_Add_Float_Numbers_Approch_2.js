b4a31717ebf5eb870216664e1a15ec7cbcf51eea
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-03-22 10:24:42 +0530

    Add files via upload

---
+++ 12_Add_Float_Numbers_Approch_2.js
@@ -0,0 +1,7 @@
+let val = parseFloat('2.3')+ parseFloat('2.4');
+console.log("2.3 + 2.4 = " + val);
+
+function gfg_Run(){
+    console.log("2.3 + 2.4 = "+Math.round((parseFloat('2.3')+parseFloat('2.4')) * 100)/100);
+}
+gfg_Run()
